0def75cf989d757ad22919175e9d449348db99a6
Kaiser Franz Josef <wecodemore@gmail.com>
2013-07-30 18:41:24 +0200

    Reformated first file.

--- internal-links.php
+++ internal-links.php
@@ -9,11 +9,8 @@ defined( 'ABSPATH' ) OR exit;
  * Version:     0.6.1
  * Text Domain: ilc
  * License:     GPL v2 @link http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-
  * (c) Copyright 2010-2013 by Franz Josef Kaiser <wecodemore@gmail.com>
  */
-
-
 
 // init class
 add_action( 'admin_init', array( 'ilcInit', 'init' ), 0 );
@@ -35,82 +32,68 @@ class ilcInit
 {
 	/**
 	 * Instance
-	 *
-	 * @access protected
-	 * @var    object
+	 * @static
+	 * @var    null|object
 	 */
-	static public $instance = null;
-
+	public static $instance = null;
 
 	/**
 	 * Used for update notices
 	 * Fetches the readme file from the official plugin repo trunk.
 	 * Adds to the "in_plugin_update_message-$file" hook
-	 *
 	 * @access public
 	 * @var    string
 	 */
 	public $remote_readme = 'http://plugins.trac.wordpress.org/browser/internal-link-checker/trunk/readme.txt?format=txt';
 
-
 	/**
 	 * Settings
-	 *
 	 * @since  0.2.2
-	 * @access public
 	 * @var    array
 	 */
 	public $args = array(
-		 'element'         => 'li'
-		,'element_class'   => ''
-		 // Att.: <ol> will be auto converted to <ul>
-		,'container'       => ''
-		,'container_class' => ''
-		,'nofollow'        => false
-		,'echo'            => true
+		'element'         => 'li',
+		'element_class'   => '',
+		// Att.: <ol> will be auto converted to <ul>
+		'container'       => '',
+		'container_class' => '',
+		'nofollow'        => false,
+		'echo'            => true,
 	);
-
 
 	/**
 	 * Container for SQL result
-	 *
 	 * @since  0.2
-	 * @access public
 	 * @var    array
 	 */
 	public $sql_results;
 
-
 	/**
 	 * Sets the meta box name
-	 * Used to determin in the extended WP_List_Table class
+	 * Used to determine in the extended WP_List_Table class
 	 * in which context the meta box is.
 	 * Needed to determine if the whole UI should be shown
-	 *
 	 * @since  0.6
-	 * @access public
 	 * @var    string
 	 */
 	public $meta_box_name = 'link-check';
 
-
 	/**
 	 * Init
 	 * Instantiates the class and loads translation files
-	 *
+	 * @static
 	 * @since  0.2
 	 * @return \ilcInit
 	 */
-	static public function init()
+	public static function init()
 	{
 		null === self::$instance AND self::$instance = new self;
+
 		return self::$instance;
 	}
 
-
 	/**
 	 * Constructor
-	 *
 	 * @since  0.2
 	 * @return \ilcInit
 	 */
@@ -126,8 +109,8 @@ class ilcInit
 			// Better update message
 			if ( 'plugins.php' === $pagenow )
 			{
-				$hook  = "in_plugin_update_message-";
-				$hook .= basename( __FILE__ )."/";
+				$hook = "in_plugin_update_message-";
+				$hook .= basename( __FILE__ ) . "/";
 				$hook .= basename( dirname( __FILE__ ) );
 				add_action( $hook, array( $this, 'update_message' ), 20, 2 );
 			}
@@ -135,7 +118,7 @@ class ilcInit
 			// abort if not on post.php (post/page/cpt edit/new) screens
 			elseif ( 'post.php' === $pagenow )
 			{
-				add_action( 'admin_init',     array( $this, 'load_extensions' ) );
+				add_action( 'admin_init', array( $this, 'load_extensions' ) );
 				add_action( 'add_meta_boxes', array( $this, 'add_meta_box' ) );
 			}
 		}
@@ -143,11 +126,10 @@ class ilcInit
 
 	/**
 	 * Load plugin translation
-	 *
 	 * @link   http://wordpress.stackexchange.com/a/33314 Translation Tutorial by the author
 	 * @return void
 	 */
-	static function load_textdomain()
+	public static function load_textdomain()
 	{
 		global $locale;
 
@@ -156,54 +138,45 @@ class ilcInit
 		$domain = 'ilc';
 
 		// in themes/plugins/mu-plugins directory
-		load_textdomain(
-			 $domain
-			,"{$dir}lang/{$domain}-{$locale}.mo"
-		);
+		load_textdomain( $domain, "{$dir}lang/{$domain}-{$locale}.mo" );
 	}
-
 
 	/**
 	 * Extension/File/Class loader
-	 *
 	 * @since  0.2.7
 	 * @return void
 	 */
 	public function load_extensions()
 	{
 		$files = array(
-			 'admin_table'
+			'admin_table'
 		);
 
 		foreach ( $files as $extension )
 		{
-			$file = plugin_dir_path( __FILE__ )."/{$extension}.php";
+			$file = plugin_dir_path( __FILE__ ) . "/{$extension}.php";
 			if ( is_readable( $file ) )
 				include_once $file;
 		}
 	}
 
-
 	/**
 	 * Plugin Header Comment data
-	 *
 	 * @since  0.2.8
-	 * @uses   get_plugin_data
 	 * @param  string $value default = 'Version'; Valid: see Header Comment Block
 	 * @param  bool   $mark_up
 	 * @return string $data
 	 */
-	static private function get_plugin_data( $value = 'Version', $mark_up = true )
+	private static function get_plugin_data( $value = 'Version', $mark_up = true )
 	{
 		$data = get_plugin_data( __FILE__, $mark_up );
+
 		return $data[ $value ];
 	}
-
 
 	/**
 	 * SQL Query
 	 * Adds content to two class vars: The resulting array & the counter
-	 *
 	 * @since  0.2
 	 * @return object $links
 	 */
@@ -212,7 +185,7 @@ class ilcInit
 		global $wpdb;
 
 		// get_permalink() cares about rewrite rules
-		$current_link = get_permalink( $GLOBALS['post']->ID );
+		$current_link = get_permalink( $GLOBALS[ 'post' ]->ID );
 		// SQL: newest first
 		$sql_results = $wpdb->get_results( $wpdb->prepare( "
 				SELECT ID, post_title, post_date, post_content, post_type
@@ -221,7 +194,7 @@ class ilcInit
 					LIKE %s
 				ORDER BY %s %s
 			" ),
-			'%'.like_escape( $current_link ).'%',
+			'%' . like_escape( $current_link ) . '%',
 			$this->orderby,
 			$this->order
 		);
@@ -229,10 +202,8 @@ class ilcInit
 		return $sql_results;
 	}
 
-
 	/**
 	 * Wrapper to return the sql results for the admin table class
-	 *
 	 * @since  0.2.7
 	 * @see    WP_List_Table::prepare_items()
 	 * @return array $sql_results
@@ -242,10 +213,8 @@ class ilcInit
 		return isset( $this->sql_results ) ? $this->sql_results : self::get_sql_results();
 	}
 
-
 	/**
 	 * Adds the meta box to the post edit screen
-	 *
 	 * @since  0.2
 	 * @return void
 	 */
@@ -260,7 +229,6 @@ class ilcInit
 			'post'
 		);
 	}
-
 
 	/**
 	 * Adds a native admin UI table
@@ -279,7 +247,6 @@ class ilcInit
 		new ilcTable( $this->meta_box_name );
 	}
 
-
 	/**
 	 * Builds the output
 	 *
@@ -287,16 +254,16 @@ class ilcInit
 	 * @uses   markup()
 	 * @return string $output
 	 */
-	function output()
+	public function output()
 	{
 		if ( ! $this->sql_results )
 			return _e( 'No posts are linking to this post.', 'ilc' );
 
 		# >>>> build links array sorted by post type
 		$results = array();
-		foreach( $this->sql_results as $post )
+		foreach ( $this->sql_results as $post )
 		{
-			$link     = get_permalink( $post->ID );
+			$link = get_permalink( $post->ID );
 
 			// If no title was set: we care about it
 			$no_title = __( 'No title set', 'ilc' );
@@ -313,22 +280,21 @@ class ilcInit
 		foreach ( $results as $name => $posts )
 		{
 			$name = _n(
-				 ucfirst( $name )
-				,ucfirst( $name ).'s'
-				,count( $posts )
-				,'ilc'
+				ucfirst( $name )
+				, ucfirst( $name ) . 's'
+				, count( $posts )
+				, 'ilc'
 			);
 			$output .= $this->markup( $posts );
 		}
 		# <<<< markup
 
 		# >>>> return
-		if ( $this->args['echo'] )
+		if ( $this->args[ 'echo' ] )
 			return print $output;
 
 		return $output;
 	}
-
 
 	/**
 	 * Builds the markup
@@ -342,53 +308,43 @@ class ilcInit
 	{
 		$output = '';
 		foreach ( $results as $link )
-		{
 			$output .= "<%EL%%EL_CLASS%>{$link}</%EL%>";
-		}
 
 		// In case someone forgot to set a container if the choosen element is 'li'
-		if ( ! $this->args['container'] OR 'li' === $this->args['element'] )
-		{
-			$output  = "<%CONTAINER%%CONTAINER_CLASS%>{$output}</%CONTAINER%>";
-		}
+		if (
+			! $this->args[ 'container' ]
+			OR 'li' === $this->args[ 'element' ]
+		)
+			$output = "<%CONTAINER%%CONTAINER_CLASS%>{$output}</%CONTAINER%>";
 
-		$output = $this->markup_filter( $output );
-
-		return $output;
+		return $this->markup_filter( $output );
 	}
-
 
 	/**
 	 * Replaces markup placeholders
 	 * Deletes placeholders if the settings array contains an empty string
-	 *
 	 * @since  0.2
 	 * @param  string $input
 	 * @return string $markup
 	 */
 	public function markup_filter( $input )
 	{
-		return strtr(
-			 $input
-			,array(
-			 	 '%EL%'              => $this->args['element']
-				 // auto correct wrong container types for <li> elements to <ul>
-				,'%CONTAINER%'       => 'li' === $this->args['element'] ? 'ul' : $this->args['container']
-				,'%EL_CLASS%'        => $this->args['element_class']    ? " class='{$this->args['element_class']}'" : ''
-				,'%CONTAINER_CLASS%' => $this->args['container_class']  ? " class='{$this->args['container_class']}'" : ''
-				,'%NOFOLLOW%'        => $this->args['nofollow']         ? ' rel="nofollow"' : ''
-			)
-		);
+		return strtr( $input, array(
+			'%EL%'              => $this->args[ 'element' ],
+			// auto correct wrong container types for <li> elements to <ul>
+			'%CONTAINER%'       => 'li' === $this->args[ 'element' ] ? 'ul' : $this->args[ 'container' ],
+			'EL_CLASS%'         => $this->args[ 'element_class' ] ? " class='{$this->args['element_class']}'" : '',
+			'%CONTAINER_CLASS%' => $this->args[ 'container_class' ] ? " class='{$this->args['container_class']}'" : '',
+			'%NOFOLLOW%'        => $this->args[ 'nofollow' ] ? ' rel="nofollow"' : ''
+		) );
 	}
 
 
 	/* =============== Helper & other ================= */
 
-
 	/**
 	 * Displays an update message for plugin list screens.
 	 * Shows only the version updates from the current until the newest version
-	 *
 	 * @since  0.2.8
 	 * @param  array  $plugin_data
 	 * @param  object $r
@@ -397,7 +353,7 @@ class ilcInit
 	public function update_message( $plugin_data, $r )
 	{
 		if ( ! is_readable( $this->remote_readme ) )
-			return;
+			return false;
 
 		// readme contents
 		$data      = file_get_contents( $this->remote_readme );
@@ -408,25 +364,19 @@ class ilcInit
 		$changelog = stristr( $changelog, "= v{$curr_ver}" );
 
 		# >>>> output
-		$output  = '<hr /><div style="font-weight: normal;">';
+		$output = '<hr /><div style="font-weight: normal;">';
 		$output .= sprintf( __(
-				 'The Update from %1$s to %2$s brings you the following new features, bug fixes and additions.'
-				,'ilc' )
-			,$curr_ver
-			,$r->new_version
+				'The Update from %1$s to %2$s brings you the following new features, bug fixes and additions.'
+				, 'ilc' )
+			, $curr_ver
+			, $r->new_version
 		);
 		$output .= "<pre>{$changelog}</pre>";
-		$output .= sprintf(
-			 __(
-				 'You can also check the nightly builds of %1$sour development repository%2$s on GitHub. If you got ideas, feature request or want to help with pull requests, please feel free to do so on GitHub.%3$s'
-				,'ilc'
-			 )
-			,'<a href="https://github.com/franz-josef-kaiser/Internal-Link-Check">'
-			,'</a>'
-			,'</div>'
+		return print $output .= sprintf(
+			__( 'You can also check the nightly builds of %1$sour development repository%2$s on GitHub. If you got ideas, feature request or want to help with pull requests, please feel free to do so on GitHub.%3$s', 'ilc' ),
+			'<a href="https://github.com/franz-josef-kaiser/Internal-Link-Check">',
+			'</a>',
+			'</div>'
 		);
-		# <<<< output
-
-		return print $output;
 	}
 } // END Class ilcInit
